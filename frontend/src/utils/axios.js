ecdd218619295dc495352bc25251a3f959283121
cdileep23 <cdileepkumar22@gmail.com>
2026-04-27 15:42:44 +0530

    [Bugfix] Wire up experiment feedback endpoints + UI integration

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1020,6 +1020,16 @@ export const endpoints = {
         `/model-hub/experiments/v2/${expId}/json-schema/`,
       getExperimentDerivedVariables: (expId) =>
         `/model-hub/experiments/v2/${expId}/derived-variables/`,
+      feedback: {
+        getTemplate: ( experimentId) =>
+          `/model-hub/experiments/v2/${experimentId}/feedback/get-template/`,
+        create: ( experimentId) =>
+          `/model-hub/experiments/v2/${experimentId}/feedback/`,
+        getDetails: ( experimentId) =>
+          `/model-hub/experiments/v2/${experimentId}/feedback/get-feedback-details/`,
+        submit: ( experimentId) =>
+          `/model-hub/experiments/v2/${experimentId}/feedback/submit-feedback/`,
+      },
     },
     apiKey: {
       create: "/model-hub/api-keys/",
