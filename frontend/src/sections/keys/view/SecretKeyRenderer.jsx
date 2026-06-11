723d1fe4b12ccc540c52ad05a892ea11ca87fe54
cdileep23 <cdileepkumar22@gmail.com>
2026-06-11 14:59:50 +0530

    fix(keys): harden SecretKey maskString against null/empty values

--- frontend/src/sections/keys/view/SecretKeyRenderer.jsx
+++ frontend/src/sections/keys/view/SecretKeyRenderer.jsx
@@ -1,6 +1,10 @@
-import { Box, Typography } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import React from "react";
 import PropTypes from "prop-types";
+import { enqueueSnackbar } from "notistack";
+import { copyToClipboard } from "src/utils/utils";
+import SvgColor from "src/components/svg-color";
+import { Events, PropertyName, trackEvent } from "src/utils/Mixpanel";
 
 const maskString = (str) => {
   const value = String(str || "");
@@ -18,11 +22,27 @@ const SecretKeyRenderer = ({ value }) => {
         display: "flex",
         alignItems: "center",
         height: "100%",
+        gap: "12px",
       }}
     >
-      <Typography variant="body2" noWrap sx={{ fontSize: 13 }}>
-        {maskString(value)}
-      </Typography>
+      {maskString(value)}
+      <IconButton
+        onClick={() => {
+          copyToClipboard(value);
+          trackEvent(Events.apikeyCopied, {
+            [PropertyName.click]: "copied",
+          });
+          enqueueSnackbar("Copied to clipboard", {
+            variant: "success",
+          });
+        }}
+        size="small"
+      >
+        <SvgColor
+          src="/assets/icons/ic_copy.svg"
+          sx={{ width: "16px", height: "16px", color: "text.primary" }}
+        />
+      </IconButton>
     </Box>
   );
 };
