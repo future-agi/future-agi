59d3d549af968289ab6eb9d8ef090cb6a618845f
N.V.J.K Kartik <n.v.j.kkartik@gmail.com>
2026-09-21 12:46:43 +0530

    fix(error-feed): merge tracer migration heads

---
+++ futureagi/tracer/migrations/0103_merge_error_feed_and_evaltask.py
@@ -0,0 +1,10 @@
+from django.db import migrations
+
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ("tracer", "0102_reconcile_report_flags"),
+        ("tracer", "0099_evaltask_reclassified_evals_revision"),
+    ]
+
+    operations = []
